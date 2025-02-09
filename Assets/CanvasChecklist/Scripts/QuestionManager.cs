b1006695d5f6fb8e5f918f930a49914175587c85
Qangan <danilfulmanf@gmail.com>
2025-02-09 16:10:57 +0700

    'lol'

--- Assets/CanvasChecklist/Scripts/QuestionManager.cs
+++ Assets/CanvasChecklist/Scripts/QuestionManager.cs
@@ -7,16 +7,18 @@ public class QuestionnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void NextQuestion(GameObject Button)
     {
-        
+
     }
+   
+
 }
